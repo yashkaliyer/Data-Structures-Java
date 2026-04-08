eb63f90a2fd5ae217dc6352f4ebf48f08c2c7029
yashkaliyer <yashkaliyer751@gmail.com>
2026-04-08 10:39:32 +0530

    add input output program for array basics

---
+++ src/Arrays/InputOutput.java
@@ -0,0 +1,18 @@
+package Arrays;
+import java.util.Scanner;
+public class InputOutput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        System.out.println("Enter the elements");
+        for(int i = 0; i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i =0; i<arr.length;i++){
+            System.out.print(arr[i]+ " ");
+        }
+
+    }
+}
